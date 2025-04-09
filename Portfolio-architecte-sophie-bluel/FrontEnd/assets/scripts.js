b30d0e010fd5afced79180c552e756270e625a76
Camille Hargez <camille.hargez@gmail.com>
2025-04-09 19:39:11 +0200

    First commit

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/scripts.js
@@ -0,0 +1,51 @@
+let projets = [
+    {
+        "image": ".assets/images/abajour-tahina.png",
+        "figcaption": "Abajour Tahina"
+    },
+    {
+        "image": ".assets/images/appartement-paris-v.png",
+		"figcaption": "Appartement Paris V"
+    },
+    {
+        "image": ".assets/images/restaurant-sushisen-londres.png",
+        "figcaption": "Restaurant Sushisen - Londres"
+    },
+    {
+        "image": ".assets/images/la-balisiere.png",
+		"figcaption": "Villa “La Balisiere” - Port Louis"
+    },
+    {
+        "image": ".assets/images/structures-thermopolis.png",
+	    "figcaption": "Structures Thermopolis"
+    },
+    {
+        "image": ".assets/images/appartement-paris-x.png",
+		"figcaption": "Appartement Paris X"
+    },
+    {
+       "image": ".assets/images/le-coteau-cassis.png",
+		"figcaption": "Pavillon “Le coteau” - Cassis"
+    },
+    {
+        "image": ".assets/images/villa-ferneze.png", 
+		"figcaption": "Villa Ferneze - Isola d’Elba"
+    },
+    {
+        "image": ".assets/images/appartement-paris-xviii.png",
+		"figcaption": "Appartement Paris XVIII"
+    },
+    {
+        "image": ".assets/images/bar-lullaby-paris.png",
+		"figcaption": "Bar “Lullaby” - Paris"
+    },
+    {
+        "image": ".assets/images/hotel-first-arte-new-delhi.png", 
+		"figcaption": "Hotel First Arte - New Delhi"
+    }
+]
+console.log(projets)
+
+let filter = document.querySelector(".filter-items")
+console.log(filter)
+
